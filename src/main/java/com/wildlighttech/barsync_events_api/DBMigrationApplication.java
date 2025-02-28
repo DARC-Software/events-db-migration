708cf2b7f6a08ddf74568f3e8f169664bfa4db23
Devin Cooper <devinraycooper@gmail.com>
2025-02-27 22:12:40 -0500

    refactor to shut down app after running

---
+++ src/main/java/com/wildlighttech/barsync_events_api/DBMigrationApplication.java
@@ -0,0 +1,25 @@
+package com.wildlighttech.barsync_events_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
+
+@SpringBootConfiguration
+@Import({DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class})
+public class DBMigrationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication application =
+				new SpringApplicationBuilder(DBMigrationApplication.class)
+						.web(WebApplicationType.NONE).build();
+
+		application.run(args);
+	}
+
+}
