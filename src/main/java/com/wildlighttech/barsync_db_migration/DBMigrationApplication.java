db47a4947115ebbc09bbf382d6997d84e9f3ef8a
Devin Cooper <devinraycooper@gmail.com>
2025-02-27 21:26:07 -0500

    rebase

---
+++ src/main/java/com/wildlighttech/barsync_db_migration/DBMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.wildlighttech.barsync_db_migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBMigrationApplication.class, args);
+	}
+
+}
