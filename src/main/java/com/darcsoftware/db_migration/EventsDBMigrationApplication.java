78fc5372c660ec469256f2323a545cf024eb3042
Devin Cooper <devinraycooper@gmail.com>
2025-10-07 03:46:31 -0400

    update url

--- src/main/java/com/darcsoftware/db_migration/EventsDBMigrationApplication.java
+++ src/main/java/com/darcsoftware/db_migration/EventsDBMigrationApplication.java
@@ -3,7 +3,6 @@ package com.darcsoftware.db_migration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,12 +10,12 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootConfiguration
 @Import({DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class})
-public class DBMigrationApplication {
+public class EventsDBMigrationApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication application =
-				new SpringApplicationBuilder(DBMigrationApplication.class)
+				new SpringApplicationBuilder(EventsDBMigrationApplication.class)
 						.web(WebApplicationType.NONE).build();
 
 		application.run(args);
--- src/test/java/com/darcsoftware/db_migration/EventsDbMigrationApplicationTests.java
+++ src/test/java/com/darcsoftware/db_migration/EventsDbMigrationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DbMigrationApplicationTests {
+class EventsDbMigrationApplicationTests {
 
 	@Test
 	void contextLoads() {
